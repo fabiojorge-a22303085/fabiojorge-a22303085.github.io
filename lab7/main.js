9c009f5bd416239868be3d73e151747ac5cbb8fa
fabiojorge-a22303085 <101054204+fabiojorge-a22303085@users.noreply.github.com>
2024-11-07 11:56:51 +0000

    upd

--- lab7/main.js
+++ lab7/main.js
@@ -1,11 +1,3 @@
-
-   let counter = 0;
-   const heading = document.querySelector('h1');
-   function count() {
-      counter++;
-      heading.textContent = counter;
-   } 
-
 // 1 
 function mudaTexto() {
     document.getElementById('transita').innerText = '1.Obrigado por passares!';
@@ -32,7 +24,7 @@ function textGreen() {
 // Definindo um array com as cores
 const cores = ['#8a8583', '#475c6c', '#eed7a1', '#cd8b62'];
 
-function alterarCorFundo() {
+function changeColor() {
     var caixaTexto = document.getElementById('caixaTexto');
     var textoLength = caixaTexto.value.length;
 
@@ -41,3 +33,26 @@ function alterarCorFundo() {
     caixaTexto.style.backgroundColor = corAtual;
 }
 
+//4
+function backgroundColor() {
+    var cor = document.getElementById('cor').value.toLowerCase();  // Obtém o valor da caixa de texto e converte para minúsculas
+
+    // Lista de cores válidas
+    var coresValidas = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'black'];
+
+    // Verifica se a cor digitada é válida
+    if (coresValidas.includes(cor)) {
+        document.body.style.backgroundColor = cor;  // Altera o fundo para a cor digitada
+    } else {
+        alert('Cor inválida! Tente novamente com uma cor válida em inglês.');
+    }
+}
+
+//5
+
+let counter = 0;
+const heading = document.querySelector('h1');
+function count() {
+   counter++;
+   heading.textContent = counter;
+} 
